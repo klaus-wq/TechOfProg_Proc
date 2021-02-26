0440576df6efa258edada47e3b76c727e91c340e
ksyuvasilek <ks8715@mail.ru>
2021-02-26 17:49:53 +0700

    Add sort

---
+++ sort.cpp
@@ -0,0 +1,35 @@
+#include "container_atd.h"
+#include "number_atd.h"
+#ifndef NULL
+#define NULL (0)
+#endif
+using namespace std;
+namespace simple_numbers {
+	//int Compare(number* first, number* second);
+    int Real(number& s);
+
+    bool Compare(number* first, number* second)
+    {
+        return Real(*first) < Real(*second);
+    }
+
+    void Sort(container& c) {
+        node* left = c.head;
+        node* right = c.head->next;
+
+        node* temp = new node;
+
+        for (int i = 0; i < c.len - 1; i++) {
+            for (int j = i + 1; j < c.len; j++) {
+                if (Compare(left->n, right->n)) {
+                    temp->n = left->n;
+                    left->n = right->n;
+                    right->n = temp->n;
+                }
+                right = right->next;
+            }
+            left = left->next;
+            right = left->next;
+        }
+    }
+} // end simple_numbers namespace
