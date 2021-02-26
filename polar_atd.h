78e1b16d3bb531e1f9cea073aeab4d70c82f8c4b
ksyuvasilek <ks8715@mail.ru>
2021-02-26 15:54:26 +0700

    Add polar_atd to add new class

---
+++ polar_atd.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef __polar_atd__
+#define __polar_atd__
+namespace simple_numbers {
+	// пол€рные координаты
+	struct polar {
+		int r; 
+		double fi; // радиус, угол
+	};
+} // end simple_numbers namespace
+#endif
