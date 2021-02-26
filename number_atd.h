6bba74f7ffea18a22914fc0e987eab7a0d89de82
ksyuvasilek <ks8715@mail.ru>
2021-02-26 15:53:08 +0700

    Update number_atd to add new key

--- number_atd.h
+++ number_atd.h
@@ -4,11 +4,12 @@
 // Подключение необходимых типов данных
 #include "complex_atd.h"
 #include "simple_atd.h"
+#include "polar_atd.h"
 namespace simple_numbers {
 	// структура, обобщающая все имеющиеся числа
 	struct number {
 		// значения ключей для каждого из числа
-		enum key { COMPLEX, SIMPLE };
+		enum key { COMPLEX, SIMPLE, POLAR };
 		key k; // ключ
 		// используемые альтернативы
 		void* obj; //используем косвенное связывание
