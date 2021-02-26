ffc6d7f8ece8abb7dab9d24a8b828f2f03f1f585
ksyuvasilek <ks8715@mail.ru>
2021-02-26 15:52:47 +0700

    Update number_Out to add new case

--- number_Out.cpp
+++ number_Out.cpp
@@ -5,6 +5,7 @@ namespace simple_numbers {
 	// Сигнатуры требуемых внешних функций.
 	void Out(complex* r, ofstream& ofst);
 	void Out(simple* t, ofstream& ofst);
+	void Out(polar* p, ofstream& ofst);
 	// Вывод параметров текущего числа в поток
 	void Out(number& s, ofstream& ofst) {
 		switch (s.k) {
@@ -13,6 +14,9 @@ namespace simple_numbers {
 			break;
 		case number::key::SIMPLE:
 			Out((simple*)s.obj, ofst);
+			break;
+		case number::key::POLAR:
+			Out((polar*)s.obj, ofst);
 			break;
 		default:
 			ofst << "Incorrect number!" << endl;
